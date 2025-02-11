70ca7a4a2f74f5bab3e4de965db94d27862a7918
leeseowoo <leeseowoo.kr@gmail.com>
2025-02-11 18:32:55 +0900

    set up spring cloud config server

--- com.springcloud.eureka.client.product/src/main/java/com/springcloud/eureka/client/product/ProductController.java
+++ com.springcloud.eureka.client.product/src/main/java/com/springcloud/eureka/client/product/ProductController.java
@@ -1,15 +1,20 @@
 package com.springcloud.eureka.client.product;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class ProductController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Value("${message}")
+    private String message;
 
     @GetMapping("/product/{id}")
     public String getProduct(@PathVariable("id") String id) {
@@ -18,6 +23,6 @@ public class ProductController {
 
     @GetMapping("/product")
     public String getProductString() {
-        return "Product port: " + serverPort;
+        return "Product port: " + serverPort + " and message: " + message;
     }
 }
